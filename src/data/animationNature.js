e625c6096156ce200ef83e195617ed8f3749045a
Lucie Caillat <lucie.caillat@gmail.com>
2023-08-29 18:18:34 +0200

    add animation data base

---
+++ src/data/animationNature.js
@@ -0,0 +1,190 @@
+import poule from "../assets/les_poules.jpg";
+
+const listAnimation = {
+  autourDeLaFerme: [
+    {
+      title: "Les petites bêtes",
+      description:
+        "Lors de cette animation les participants découvriront le monde des petites bêtes, les différentes familles et leurs caractéristiques avant de les observer grâce à une « chasse » en extérieur ou dans du compost ramené. Pour finir le rôle écologique des petites bêtes sera évoqué au travers d’un conte et la fabrication d’un abris à insectes pourra être proposée. ",
+      img: poule,
+      goals: [
+        "Connaître les différentes classes de macro-invertébrés",
+        "Comprendre le rôle écologique des macro-invertébrés",
+        "Aménager des abris pour la faune auxiliaire",
+      ],
+    },
+
+    {
+      title: "Balade nature / Trésors cachés ",
+      description:
+        "Lors d’une balade nature sur un site défini, les participants apprendront à observer et reconnaître les plantes et arbres présents. Pour les plus petits, une chasse au trésors peut être organisée pour rendre ce moment encore plus ludique ! ",
+      img: poule,
+      goals: ["Découvrir quelques arbres et plantes"],
+    },
+
+    {
+      title: "L’univers de la forêt",
+      description:
+        "Une balade explicative au cœur d’une forêt pour découvrir le milieu forestier, le rôle des arbres et leur cycle de vie. ",
+      img: poule,
+      goals: [
+        "Découvrir le milieu forestier",
+        "Découvrir le rôle des arbres",
+        "Découvrir le cycle de vie des arbres",
+      ],
+    },
+
+    {
+      title: "Le monde des oiseaux ",
+      description:
+        "Lors de cet atelier les participants découvrirons les oiseaux, leur morphologie et leur cycle de vie avant de tenter de les observer en extérieur. Pour finir, ils pourront s’atteler à la fabrication commune d’un nichoir, ou de boules de graisse. ",
+      img: poule,
+      goals: [
+        "Aborder la notion de biodiversité",
+        "Reconnaître quelques oiseaux et comprendre leur cycle de vie",
+        "Apprendre à les observer et les protéger",
+      ],
+    },
+
+    {
+      title: "Traces et indices",
+      description:
+        "Découverte et recherches des indices de présences des animaux. Étude de leur mode de vie ainsi que de leur alimentation.",
+      img: poule,
+      goals: [
+        "Découvrir les principaux animaux sauvages de notre environnement",
+        "Découvrir leurs modes de vie",
+        "Reconnaître les indices de leur présence et leurs traces",
+        "Découvrir les concepts de chaînes alimentaires et de biodiversité",
+      ],
+    },
+
+    {
+      title: "La nature et l’art",
+      description:
+        "La nature est une source d’éveil et d’inspiration. Cette animation permettra aux participants de laisser libre court à leur imagination pour créer, jouer et composer avec des éléments naturels! Land art, musique verte, jeux buissonniers… de nombreuses activités sont possibles !",
+      img: poule,
+      goals: [
+        "Réaliser et créer des objets/ jeux ou œuvres à partir d’éléments collectés dans la nature",
+        "Développer son imagination",
+      ],
+    },
+
+    {
+      title: "L’univers de la mare ",
+      description:
+        "Qui sont les habitants de la mare ? Les participants, munis d’une épuisette partiront à la recherche de la faune et la flore de ce milieu fragile et découvrirons le rôle écologique important qu’elles jouent !",
+      img: poule,
+      goals: [
+        "Reconnaître un insecte",
+        "Faire un inventaire non exhaustif de la faune et de la flore de la mare",
+      ],
+    },
+  ],
+
+  autourDeLaNature: [
+    {
+      title: "Découverte des animaux",
+      description:
+        "Découverte des animaux grâce à des ateliers et la rencontre avec les animaux visiteurs. ",
+      img: poule,
+      goals: [
+        "Reconnaître les animaux, savoir les nommer, les toucher",
+        "Pour chaque animal, connaître : son alimentation, son milieu de vie, son utilité, ses principales caractéristiques physiques",
+        "Comprendre le cycle de reproduction en nommant le mâle, la femelle et le petit",
+      ],
+    },
+
+    {
+      title: "De l’œuf à la poule !",
+      description:
+        "Une approche ludique et imaginaire pour découvrir les poules et le mode de reproduction ovipare. Après une présentation des poules en visite, les participants découvriront, au travers de petits ateliers, la composition de l’œuf , le développement du poussin à l'intérieur et le cycle de vie des poules.",
+      img: poule,
+      goals: [
+        "Acquérir le vocabulaire lié aux poules et au poulailler",
+        "Rencontrer les poules",
+        "Voir la composition de l’œuf et le développement du poussin à l'intérieur",
+        "Apprendre le cycle de vie de la poule",
+        "Comprendre le mode de reproduction ovipare",
+      ],
+    },
+
+    {
+      title: "La laine et le mouton ",
+      description:
+        "Découverte du mouton et approche sensorielle autour de la laine. Par de petites expériences les participants découvriront ses propriétés puis ils apprendront à la carder , à aligner ses fibres et faire un fil, le tout en chanson !",
+      img: poule,
+      goals: [
+        "Acquérir le vocabulaire lié aux moutons et à la laine",
+        "Identifier les différentes étapes du cycle de transformation de la laine ",
+      ],
+    },
+
+    {
+      title: "Les produits laitiers",
+      description:
+        "Découverte de l’origine du lait, dégustation de laits et expérimentations sur la création de fromage et de beurre.",
+      img: poule,
+      goals: [
+        "Comprendre l'origine du lait",
+        "Comprendre les procédés de transformation du lait : de la traite aux produits laitiers",
+        "Expérimenter la fabrication de fromage et de beurre",
+      ],
+    },
+  ],
+
+  autourDuDD: [
+    {
+      title: "L’aventure sensorielle dans l’alimentation",
+      description:
+        "À travers des ateliers sensoriels et ludiques autour de l'alimentation les participants découvrent ou redécouvrent leurs 5 sens, s'ouvrent à l'écoute des stimulations corporelles et environnementales, apprennent à décrire leur ressentis et à envisager les plaisirs alimentaires autrement.",
+      img: poule,
+      goals: [
+        "Découvrir l'alimentation par ses sens",
+        "Comprendre l'intérêt de l'alimentation",
+        "Connaître les différentes catégories d'aliments",
+      ],
+    },
+    {
+      title: "Fruits et légumes de saison",
+      description:
+        "Cette animation donne l’occasion aux participants de s’interroger sur la provenance des aliments et de découvrir des fruits et légumes locaux et de saison. Les besoins et le cycle de croissance des plantes sont abordés. Pour finir, une petite plantation individuelle peut être proposée ! ",
+      img: poule,
+      goals: [
+        "Découverte des fruits et légumes et de la notion de saisonnalité",
+        "Sensibiliser aux cycles des plantes du potager",
+      ],
+    },
+    {
+      title: "Crêpe party",
+      description:
+        "Quels sont les différentes catégories d'aliments, d'où viennent-ils et comment les fabrique-t'on ? Autant de questions qui trouveront leurs réponses à l'aide de petits jeux et de la réalisation de crêpes que les participants prendront plaisir à déguster. Pour finir nous nous interrogerons sur le devenir de tous les déchets produits lors de l'atelier.",
+      img: poule,
+      goals: [],
+    },
+    {
+      title: "",
+      description: "",
+      img: poule,
+      goals: [
+        "Identifier le produit brut dont sont issus les aliments",
+        "Différencier les aliments selon leur origine animale ou végétale",
+        "Comprendre le procédé de fabrication de certains aliments",
+        "Réaliser une recette simple et partager un goûter",
+      ],
+    },
+    {
+      title: "La seconde vie de nos déchets,",
+      description:
+        "Que deviennent nos déchets ? Pourquoi et comment les trier ? Quel est le cycle des différentes matières et comment réduire nos déchets à travers nos choix de consommation ? Autant de questions qui trouveront des réponses lors de cet atelier !",
+      img: poule,
+      goals: [
+        "Appréhender la notion de déchets",
+        "Appréhender le cycle des déchets, les différentes matières",
+        "Prendre conscience de la possibilité de réduire la quantité de nos déchets à travers nos choix de consommation",
+      ],
+    },
+  ],
+};
+
+export default listAnimation;
