89ab5cf5acce5036bbd4b4db00e6bff047e40afd
Lucie Caillat <lucie.caillat@gmail.com>
2023-09-04 16:52:24 +0200

    add GlobalStyle component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesApp from "./Routes/RoutesApp";
+import GlobalStyle from "./globalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Router>
       <RoutesApp />
     </Router>
