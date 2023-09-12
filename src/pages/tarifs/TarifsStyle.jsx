79916ba7c6d22c0954764ac401575ee208dfe614
Lucie Caillat <lucie.caillat@gmail.com>
2023-09-12 11:51:57 +0200

    new Tarifs page style 2

--- src/pages/tarifs/TarifsStyle.jsx
+++ src/pages/tarifs/TarifsStyle.jsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 import colors from "../../utils/style/colors";
 
 export const TarifsContainer = styled.div`
-  background-color: ${colors.cream};
-  padding: 30px 0 0;
+  background: linear-gradient(${colors.offWhite}, ${colors.cream});
+  padding: 20px 0 0;
   & h1 {
-    margin: 0 3%;
+    margin: 0 5%;
+    color: ${colors.darkGray};
   }
   & .tarif_contactez_moi {
     color: ${colors.orange};
@@ -26,21 +27,21 @@ export const TarifsBoxesContainer = styled.div`
 `;
 
 export const TarifsBox = styled.section`
-  &.tarifs_anim_nat h2 {
-    color: ${colors.darkGreen};
+  &.tarifs_anim_nat {
+    background-color: ${colors.green};
   }
-  &.tarifs_autour_chien h2 {
-    color: ${colors.orange};
+  &.tarifs_autour_chien {
+    background-color: ${colors.brown};
   }
-  &.tarifs_médiation_a h2 {
-    color: ${colors.purple};
+  &.tarifs_médiation_a {
+    background-color: ${colors.lightBrown};
   }
-  &.tarifs_particuliers h2 {
-    color: ${colors.blue};
+  &.tarifs_particuliers {
+    background-color: ${colors.darkGreen};
   }
   width: 48%;
   min-width: 700px;
-  background-color: ${colors.offWhite};
+  color: ${colors.offWhite};
 
   padding: 20px 30px;
   margin: 40px 0;
@@ -50,18 +51,15 @@ export const TarifsBox = styled.section`
   & p {
     margin: 20px 0;
     text-align: left;
-    color: ${colors.darkGray};
   }
   & .frais {
-    color: ${colors.darkGray};
-    filter: brightness(0.7);
+    color: white;
     text-align: center;
     margin: 40px 0;
     transform: scale(1.2);
   }
   & div {
     font-weight: normal;
-    color: ${colors.darkGray};
   }
   @media screen and (max-width: 1535px) {
     width: 90%;
